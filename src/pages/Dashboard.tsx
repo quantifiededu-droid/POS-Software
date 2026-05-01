30bef5bb7cb85474e8853a3ccd8e2042b87d00a9
quantifiededu-droid <quantifiededu@gmail.com>
2026-05-01 23:26:34 +0000

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -20,7 +20,7 @@ import {
   ResponsiveContainer,
   CartesianGrid,
 } from 'recharts';
-import { cn } from '../lib/utils';
+import { cn } from '../components/UI';
 
 export default function Dashboard() {
   const navigate = useNavigate();
